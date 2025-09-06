1bc76f665890aadc80323635e5d089ef1865db55
William Boothe <89033570+WilliamBoothe@users.noreply.github.com>
2025-09-06 01:52:49 +0000

    All done! I should have committed more but I got lost in the trial and error

--- script.js
+++ script.js
@@ -1,17 +1,56 @@
-const rows = 16;
-const cols = 16;
-const mainGrid = document.querySelector("#mainGrid");
+const gridMin = 1;
+const gridMax = 100;
+let gridSize = 50;
+buildGrid(gridSize);
 
-for (let r = 0; r < rows; r++) {
-  const cellRow = document.createElement("div");
-  cellRow.className = "cellRow";
-  for (let c = 0; c < cols; c++) {
-    const cell = document.createElement("div");
-    cell.className = "cell notHovered";
-    cell.addEventListener("mouseover", () => {
-      cell.className = "cell hovered";
-    });
-    cellRow.appendChild(cell);
+// Button to change grid size
+const adjustGrid = document.querySelector("button#adjustGrid");
+adjustGrid.addEventListener("click", () => {
+  const userInput = promptUserForGridSize();
+  if (userInput === null) return;
+  buildGrid(userInput);
+});
+
+function buildGrid(size) {
+  const mainGrid = document.querySelector("div#mainGrid");
+  mainGrid.replaceChildren();
+
+  for (let r = 0; r < size; r++) {
+    const cellRow = document.createElement("div");
+    cellRow.className = "cellRow";
+    for (let c = 0; c < size; c++) {
+      const cell = document.createElement("div");
+      cell.className = "cell";
+      cell.addEventListener("mouseover", () => {
+        cell.className = "cell active";
+      });
+      cellRow.appendChild(cell);
+    }
+    mainGrid.appendChild(cellRow);
   }
-  mainGrid.appendChild(cellRow);
+}
+
+function promptUserForGridSize() {
+  let userInput = prompt(
+    "Choose the cell count for each axis (whole number between 1 and 100):",
+    gridSize
+  );
+  if (userInput === null) return null; // User cancelled input
+  userInput = Number(userInput);
+  const inputValid = validateGridSizeInput(userInput);
+
+  if (inputValid) return userInput;
+  alert("Somebody doesn't follow directions!");
+  return null;
+}
+
+function validateGridSizeInput(userInput) {
+  let isValid = true;
+
+  if (userInput === null) isValid = false;
+  if (!Number.isInteger(userInput)) isValid = false;
+  if (userInput < gridMin) isValid = false;
+  if (userInput > gridMax) isValid = false;
+
+  return isValid;
 }
